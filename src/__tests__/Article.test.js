28439559ff9b7aa0ae54e0448b5d76c9955d833c
pknowledge <jamesblssings@gmail.com>
2025-07-09 09:23:12 +0300

    changes on the article and header

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -8,9 +8,9 @@ function Article() {
 export default Article;
 
 // src/__tests__/Article.test.js
+import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import React from "react";
 import Article from "../components/Article"; // Ensure the path is correct
 
 test("displays the text 'please pass this test'", () => {
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -17,9 +17,9 @@ function Header() {
 
 export default Header;
 // src/__tests__/Header.test.js
+import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import React from "react";
 import Header from "../components/Header"; // Ensure the path is correct
 
 test("displays the text 'hello from the Header!'", () => {
